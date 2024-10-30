81f6ae3518bca788642a0ebc0620d718f0cb8e0b
Ben <87001491+BenCurtisD@users.noreply.github.com>
2024-10-30 13:08:34 +1100

    Update arducar_com.py

--- arducar_com.py
+++ arducar_com.py
@@ -1,30 +1,6 @@
 import serial
 import time
 
-# ------------------------------------------------------------
-#
-# DEFAULT VALUES - ONLY CHANGE THIS TO MATCH FROM THE MODEL OUTPUT. Ben, Joe, Kevin
-#
-# ------------------------------------------------------------
-
-class_output = 0
-action = "S"
-speed = "0"
-
-# ------------------------------------------------------------
-#
-# SWITCH CASES: Edward will Write this part.
-#
-# ------------------------------------------------------------
-
-
-
-# ------------------------------------------------------------
-#
-# SENDING COMMANDS
-#
-# ------------------------------------------------------------
-
 # Update 'COMX' with the port that corresponds to your HC-05 module
 bluetooth = serial.Serial(port='COM7', baudrate=9600, timeout=1)
 
@@ -44,27 +20,25 @@ def update_car(model_output: int):
         action, speed = action_lookup[model_output]
     except IndexError:
         action, speed = "S", "0"
+
     send_message(action)
     send_message(speed)
 
-def send_message(message):
+def send_message(message: str):
     bluetooth.write(message.encode())  # Send message as bytes
     time.sleep(0.1)                    # Small delay to allow processing
 
 if __name__ == "__main__":
     try:
+        update_car(0)
 
-        send_message(action)
-        send_message(speed)
-
-        update_car(-1)
-
-        def live_command_test():
-            while (True):
-                command = input("Enter: ")
-                send_message(command)
-
-        #####live_command_test()
+        while True:
+            try:
+                test_model_output = int(input("Enter for function update_car: "))
+            except ValueError:
+                print("Please only enter a number between 0 and 8")
+                continue
+            update_car(test_model_output)
 
     finally:
         bluetooth.close()  # Ensure the Bluetooth connection is closed after use
